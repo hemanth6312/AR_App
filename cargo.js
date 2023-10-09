eb1951869b17b1202ef840ad40e6fc216fb54229
Glitch (glitch-hello-website) <none>
2023-10-09 06:08:45 +0000

    🎒🌈 Checkpoint ./cargo.html:45268477/6682 ./cargo.js:45268477/431 ./cargo.css:45268477/4229 ./index.html:45268477/48

---
+++ cargo.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
